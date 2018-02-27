7f181fe36176a06c31792b9dc50614d4e49c77cc
Shengda Liu <liu.shengda@orange.com>
2018-02-27 15:50:49 +0100

    multiple offers managed

--- src/components/OfferComponent.js
+++ src/components/OfferComponent.js
@@ -6,6 +6,29 @@ import axios from 'axios';
 
 require('styles//Offer.css');
 
+var api_url = 'http://localhost:8081/test';
+
+
+function starFormat(fieldValue, row) {
+  var icon = '<img src="../images/star.svg">';
+  if (row.star == 2) {
+    icon = '<img src="../images/star.svg"><img src="../images/star.svg">';
+  }
+  if (row.star == 3) {
+    icon = '<img src="../images/star.svg"><img src="../images/star.svg"><img src="../images/star.svg">';
+  }
+  return icon;
+}
+
+function offerFormatter(cell, row) {
+  let offerShow = `${cell}`;
+  if (row.specialOffers.length > 1)
+  {
+    offerShow = 'More than one offer, click to see details';
+  }
+  return offerShow;
+}
+
 class OfferComponent extends React.Component {
 
   constructor(props) {
@@ -20,20 +43,47 @@ class OfferComponent extends React.Component {
   }
 
   OfferComponent() {
-    axios.get('http://localhost:8081/test')
+    axios.get(api_url)
       .then(res => {
         const offers = res.data;
         this.setState({ offers });
       });
   }
 
+  isExpandableRow(row) {
+    if (row.specialOffers.length > 1) return true;
+    else return false;
+  }
+
+  expandComponent(row) {
+    let offers = [];
+
+    row.specialOffers.forEach((offer, index) => {
+      offers.push({
+        offer_id: index,
+        offer_descrption: offer
+      })
+    });
+
+    return (
+      <BootstrapTable data={offers}  version='4'>
+          <TableHeaderColumn isKey dataField='offer_id' dataSort={ true } width='100'>Id</TableHeaderColumn>
+          <TableHeaderColumn dataField='offer_descrption'>Offer Detail</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+
   render() {
     var listOffers = this.state.offers;
 
-    return (<BootstrapTable data={listOffers}  version='4'>
-          <TableHeaderColumn isKey dataField='name' dataSort={ true }>Product ID</TableHeaderColumn>
-          <TableHeaderColumn dataField='star' dataSort={ true }>Product Name</TableHeaderColumn>
-          <TableHeaderColumn dataField='chef' dataSort={ true }>Product Price</TableHeaderColumn>
+    return (<BootstrapTable exportCSV data={listOffers} version='4'
+      expandableRow={ this.isExpandableRow }
+      expandComponent={ this.expandComponent }
+      search>
+          <TableHeaderColumn isKey dataField='name' dataSort={ true }>Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='star' dataSort={ true } dataFormat={ starFormat } width='100'>Star</TableHeaderColumn>
+          <TableHeaderColumn dataField='chef' dataSort={ true }>Chef's Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='specialOffers' dataFormat={ offerFormatter } >Offers</TableHeaderColumn>
       </BootstrapTable>)
   }
 }
